640ab38ea684345f8eaf4c5b29b245fd2e9f49a8
Ekaterina <ekaterina_r21@mail.ru>
2022-05-04 19:52:41 +0300

    Задача с вещественными числами

---
+++ Task38/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите
+//разницу между максимальным и минимальным
+//элементов массива.
+void FillArray(double[]array, int A, int B)
+{
+    for(int i=0; i<array.Length; i++)
+    {
+        array[i] = new Random().Next(A, B)+ new Random().NextDouble(); 
+    }
+}
+void PrintArray(double[] array)
+{
+    for(int i=0; i< array.Length; i++)
+    {
+        Console.Write(Math.Round(array[i],2) + " ");
+    }
+}
+double[]array = new double[5];
+FillArray(array, 0, 10);
+PrintArray(array);
