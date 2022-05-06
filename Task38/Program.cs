e4b7188760fff4fff4759628e60a893a59e97064
Ekaterina Rudneva <103060211+rudninka@users.noreply.github.com>
2022-05-06 13:03:13 +0300

    переделала задачу

--- Task38/Program.cs
+++ Task38/Program.cs
@@ -18,3 +18,19 @@ void PrintArray(double[] array)
 double[]array = new double[5];
 FillArray(array, 0, 10);
 PrintArray(array);
+Console.WriteLine();
+double min = array[0];
+double max = array[array.Length - 1];
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] < min)
+    {
+        min = array[i];
+    }
+    else if (array[i] > max)
+    {
+        max = array[i];
+    }
+}
+Console.Write($"Максимальный элемент массива {max}, а минимальный {min}");
+Console.Write($"Разница между наибольшим и наименьшим элементами {max - min}");
